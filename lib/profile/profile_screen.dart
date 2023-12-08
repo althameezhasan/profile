3bd3960cce29a875c381b569a396a20aa37ea345
unknown <azemaltha@gmail.com>
2023-12-08 12:50:52 +0530

    the change

---
+++ lib/profile/profile_screen.dart
@@ -0,0 +1,321 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+  // final List<String> vehicles = ['Car', 'Motorcycle', 'Bicycle', 'Truck'];
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final List<String> vehicles = ['Car', 'Motorcycle', 'Bicycle', 'Truck'];
+  String buttonLabel = "Update ";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.green,
+          ),
+          onPressed: () {},
+        ),
+        title: Text(
+          'Profile',
+          style: TextStyle(
+            color: Colors.green,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.green,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 30, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("AssetsImage/user_image.jpg"),
+                          )),
+                    ),
+                    Positioned(
+                        // top: 10,
+                        bottom: 10,
+                        right: 0,
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 4,
+                              color: Colors.green,
+                            ),
+                            color: Colors.green,
+                          ),
+                          child: Icon(
+                           Icons.edit,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Full Name",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                      height:
+                          8), // Adjust the spacing between label and text field
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 143, 139, 139),
+                        width: 2.0,
+                      ),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.only(left: 10, right: 10, bottom: 3),
+                        hintText: "Althameez",
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: const Color.fromARGB(255, 143, 139, 139),
+
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "Mobile Number",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                      color: const Color.fromARGB(255, 143, 139, 139),
+
+                        width: 2.0,
+                      ),
+                    ),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.only(left: 10, right: 10, bottom: 3),
+                        hintText: "Enter your mobile number",
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: const Color.fromARGB(255, 143, 139, 139),
+
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    " Enter E-Mail",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                  color: const Color.fromARGB(255, 143, 139, 139),
+
+                        width: 2.0,
+                      ),
+                    ),
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      // inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        } else if (!RegExp(
+                                r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
+                            .hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.only(left: 10, right: 10, bottom: 3),
+                        hintText: "Abc@gmail.com",
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                         color: const Color.fromARGB(255, 143, 139, 139),
+
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "Select your vehicle",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                  color: const Color.fromARGB(255, 143, 139, 139),
+
+                        width: 2.0,
+                      ),
+                    ),
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      underline: SizedBox(), // Remove the underline
+                      value: null, // You can set the default value if needed
+                      hint: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text("Select your vehicle"),
+                      ),
+                      onChanged: (String? value) {
+                        // Handle dropdown value changes
+                      },
+                      items: vehicles.map((String vehicle) {
+                        return DropdownMenuItem<String>(
+                          value: vehicle,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text(vehicle),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+
+                  Text(
+                    "Your Liscence Plate",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                      height:
+                          8), // Adjust the spacing between label and text field
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                     color: const Color.fromARGB(255, 143, 139, 139),
+
+                        width: 2.0,
+                      ),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.only(left: 10, right: 10, bottom: 3),
+                        hintText: "NY 3271",
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                       color: const Color.fromARGB(255, 143, 139, 139),
+
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+
+                  // Button with dynamic label
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Update the label when the button is pressed
+                        setState(() {
+                          buttonLabel = "Label Updated!";
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                            Colors.green, // Set the background color to green
+                      ),
+                      child: Text(
+                        buttonLabel,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

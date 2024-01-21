3c7c1a2665cc8d06cd5d66996f6de5583dd2056c
althameezhasan <131872964+althameezhasan@users.noreply.github.com>
2024-01-21 13:35:18 +0530

    thechanges

---
+++ lib/SignInScreens/verification_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class VerificationPage extends StatefulWidget {
+  const VerificationPage({super.key});
+
+  @override
+  State<VerificationPage> createState() => _VerificationPageState();
+}
+
+class _VerificationPageState extends State<VerificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () => Get.back(),
+              icon: const Icon(CupertinoIcons.clear))
+        ],
+      ),
+      body: const SingleChildScrollView(),
+    );
+  }
+}
